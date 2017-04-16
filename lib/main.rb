4f4c3e561a2c118a4a6c2c7ff43fd8cca91b9857
Partho Ghosh <partho.ghosh24@gmail.com>
2017-04-16 12:23:46 +0530

    Added entry main, GameWindow and utils

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,22 @@
-require "tanx/version"
+require "gosu"
 
-module Game
-  # Your code goes here...
+root_dir = File.dirname(__FILE__)
+required_pattern=File.join(root_dir,'**/*.rb')
+@failed = []
+
+Dir.glob(required_pattern).each do |file|
+  next if file.end_with?('/main.rb')
+  begin
+    require_relative file.gsub("#{root_dir}",'')
+  rescue ExceptionName
+    @failed << file
+  end
 end
+
+@failed.each do |file|
+  require_relative file.gsub("#{root_dir}",'')
+end
+
+$window = GameWindow.new
+GameState.switch(MenuState.instance)
+$window.show
